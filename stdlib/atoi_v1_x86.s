491296794e8f067fef6622e27c9279f588c9cb40
Dan Hutchinson <d.hutchinson4@googlemail.com>
2014-03-02 13:29:25 +0000

    Implementation of atoi with error checking for non-numeric ASCII codes.

--- stdlib/atoi_v1_x86.s
+++ stdlib/atoi_v1_x86.s
@@ -1,12 +1,96 @@
 ; atoi_v1_asm.s - Anything to integer
 
+; ASCII codes for minus, 0 and 9
+%define MINUS 45
+%define ASCII_ZERO 48
+%define ASCII_NINE 58
+
 global SYM_NAME
 
 SYM_NAME:
 	push ebp
 	mov ebp, esp
 
-	
+	; ecx will contain the address of the string
+	; ebx will contain the power of 10
+	; eax will contain the value
+	; edi will contain a 'strach' area
 
+nav_to_start:
+	; Step 1, we need to find the start of the string, here we know
+	; 10^0.
+	; Use scasb for this...
+	; edi is compared to al and ecx is decremented each time.
+	push edi
+	mov edi, [ebp+8]
+
+	; ecx is the max count, i.e. where to stop (which is 2^32)
+	xor ecx, ecx
+	not ecx
+
+	; al is compared, so set to 0
+	xor al, al
+
+	cld
+	repne scasb
+
+	; TODO: clean this up.
+	; ecx has been decremented each time through the loop, so now we're
+	; one past the end of the array so figure out the offset from the
+	; start of the array.
+	not ecx
+	lea ecx, [ecx-2]
+
+	mov edi, [ebp+8]
+	add edi, ecx
+	mov ecx, edi
+
+	; Save ebx (our pow 10, and 0 out the variables)
+	push ebx
+	mov ebx, 1
+
+	xor eax, eax
+atoi_loop:
+	mov edi, [ecx]
+	and edi, 0x000000FF
+
+	; Conditions
+	; 1). is edi == '-'
+	cmp edi, MINUS
+	je is_minus
+
+	; 2). < 48
+	cmp edi, ASCII_ZERO
+	jl on_error
+
+	; 3). > 58
+	cmp edi, ASCII_NINE
+	jg on_error
+
+	; Normalise the value
+	sub edi, ASCII_ZERO
+	; Increase to approrpiate power
+	imul edi, ebx
+	; Increment the power
+	imul ebx, 10
+	add eax, edi
+
+	dec ecx
+	cmp ecx, [ebp+8]
+	jge atoi_loop
+	jmp finish
+
+on_error:
+	mov eax, -1
+	jmp finish
+
+is_minus:
+	neg eax
+	jmp finish
+	
+finish:
+	pop ebx
+	pop edi
 	pop ebp
+
 	ret
--- tests/atoi_driver.c
+++ tests/atoi_driver.c
@@ -4,18 +4,30 @@
 
 extern int atoi_asm_v1(const char* string);
 
-const char numbers[10][2] = {
-	{'0', '\0'},
-	{'1', '\0'},
-	{'2', '\0'},
-	{'3', '\0'},
-	{'4', '\0'},
-	{'5', '\0'},
-	{'6', '\0'},
-	{'7', '\0'},
-	{'8', '\0'},
-	{'9', '\0'}
+const char numbers[20][10] = {
+	{"0"},
+	{"1"},
+	{"2"},
+	{"3"},
+	{"4"},
+	{"5"},
+	{"6"},
+	{"7"},
+	{"8"},
+	{"9"},
+	{"20"},
+	{"130"},
+	{"9543"},
+	{"11"},
+	{"-5"},
+	{"-432"},
+	{"-22"},
+	{"afg"},		// this should return error
+	{"32876"},
+	{"-32876"},
 };
+
+#define NUM_STRINGS sizeof(numbers)/10
 
 int main(int argc, char **argv)
 {
@@ -23,15 +35,17 @@ int main(int argc, char **argv)
 	{
 		printf("Specify [C | v1 | v2 | v3]\n");
 		return -1;
-	} 
+	}
 
 	if ( strcmp(argv[1], "C" ) == 0)
 	{
 		printf("C TEST\n");
 		int str_index = 0;
-		for(; str_index < 10; ++str_index)
+		int value = 0;
+		for(; str_index < NUM_STRINGS; ++str_index)
 		{
-			printf("%d\n", atoi(numbers[str_index]));
+			value = atoi(numbers[str_index]);
+			printf("%d, *10 = %d\n", value, value * 10);
 		}
 
 	}
@@ -39,11 +53,13 @@ int main(int argc, char **argv)
 	{
 		printf("ASM v1 TEST\n");
 		int str_index = 0;
-		for(; str_index < 10; ++str_index)
+		int value = 0;
+		for(; str_index < NUM_STRINGS; ++str_index)
 		{
-			printf("%d\n", atoi_asm_v1(numbers[str_index]));
+			value = atoi_asm_v1(numbers[str_index]);
+			printf("%d, *10 = %d\n", value, value * 10);
 		}
 	}
-	
+
 	return 0;
 }
